93b5da5b0a5e2487cb27e93f00cfdc61d1274251
Olonde Felix <olondenarkiso@gmail.com>
2020-09-23 12:29:52 +0300

    For loop introduced

--- main.py
+++ main.py
@@ -21,112 +21,15 @@ current_field = [[" ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " "],
 connect4(current_field)
 while(True):
   print('Players turn: ', player)
-  move_row = -1
   move_column = int(input('Please Enter the column:\n'))
   if player == 1:
-    if move_column == 0:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'X'
-        player = 2
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'X'
-        player = 2
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'X'
-        player = 2
-      else:
-        current_field[move_column][0] = 'X'
-        player = 2
-    elif move_column == 1:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'X'
-        player = 2
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'X'
-        player = 2
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'X'
-        player = 2
-      else:
-        current_field[move_column][0] = 'X'
-        player = 2
-    elif move_column == 2:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'X'
-        player = 2
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'X'
-        player = 2
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'X'
-        player = 2
-      else:
-        current_field[move_column][0] = 'X'
-        player = 2
-    elif move_column == 3:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'X'
-        player = 2
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'X'
-        player = 2
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'X'
-        player = 2
-      else:
-        current_field[move_column][0] = 'X'
+    for col in range(1, 7):
+      if current_field[move_column][-1] == " ":
+        current_field[move_column][-1] = 'X'
         player = 2
   else:
-    if move_column == 0:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'O'
-        player = 1
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'O'
-        player = 1
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'O'
-        player = 1
-      else:
-        current_field[move_column][0] = 'O'
-        player = 1
-    elif move_column == 1:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'O'
-        player = 1
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'O'
-        player = 1
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'O'
-        player = 1
-      else:
-        current_field[move_column][0] = 'O'
-        player = 1
-    elif move_column == 2:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'O'
-        player = 1
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'O'
-        player = 1
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'O'
-        player = 1
-      else:
-        current_field[move_column][0] = 'O'
-        player = 1
-    elif move_column == 3:
-      if current_field[move_column][move_row] == " ":
-        current_field[move_column][move_row] = 'O'
-        player = 1
-      elif current_field[move_column][move_row] != " ":
-        current_field[move_column][move_row - 1] = 'O'
-        player = 1
-      elif current_field[move_column][move_row - 1] != " ":
-        current_field[move_column][move_row - 2] = 'O'
-        player = 1
-      else:
-        current_field[move_column][0] = 'O'
+    for col in range(1, 7):
+      if current_field[move_column][-1] == " ":
+        current_field[move_column][-1] = 'O'
         player = 1
   connect4(current_field)
